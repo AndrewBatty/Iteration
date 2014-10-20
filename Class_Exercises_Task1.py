2ff6c14d43aaec7e4d83d4787f0e052aff83b743
Andrew Batty <21553@longroad.ac.uk>
2014-10-20 12:16:36 +0100

    Iteration Revision exercises 1 and 2

---
+++ Class_Exercises_Task1.py
@@ -0,0 +1,9 @@
+# Andrew Batty
+# 20/10/14
+# Iteration class Exercices: Task 1:
+
+total = 0
+for count in range(1,9):
+    number = int(input("Please enter a number: "))
+    total = total + number
+print(total)
---
+++ Iteration_Revision_Exercise1.py
@@ -0,0 +1,6 @@
+# Andrew Batty
+# 20/10/14
+# Iteration Revision exercises 1:
+
+for count in range(10):
+    print("Hello World")
